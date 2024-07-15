e689e64db0d4101f104f7dafc2263d1932ce00fb
Mangala DasJenkins <das.prasanna.mangala@gmail.com>
2024-07-15 17:41:51 +0530

    New Google Test

---
+++ src/test/java/Amazon/FloraRelease/GoogleTest.java
@@ -0,0 +1,32 @@
+package Amazon.FloraRelease;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+	
+
+	WebDriver driver;
+
+	@Test(priority = 0)
+	public void amazonLaunchTest() {
+		driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.in");
+		driver.manage().window().maximize();
+		// The comment
+
+	}
+
+	
+
+	@AfterClass
+	public void browserClose() {
+		driver.quit();
+	}
+}
