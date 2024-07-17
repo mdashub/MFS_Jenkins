2a28073b22cad7859cec814a4fbd87f706f9e896
Mangala DasJenkins <das.prasanna.mangala@gmail.com>
2024-07-17 14:41:41 +0530

    Parameter added for mvn to use it

--- src/test/java/Amazon/FloraRelease/GoogleTest.java
+++ src/test/java/Amazon/FloraRelease/GoogleTest.java
@@ -8,14 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
 
 	WebDriver driver;
 
+
 	@Test(priority = 0)
-	public void amazonLaunchTest() {
+	@Parameters("paraMdas")
+	public void amazonLaunchTest(@Optional String defaultValue_Hai, String localparaMdas) {
 
 		boolean remote = true;
 		if (remote)
@@ -23,7 +27,9 @@ public class GoogleTest {
 
 		else
 			driver = new ChromeDriver();
-
+		System.out.println(
+				"***************************************************Passing Params from jenkins********************************************************************************** "
+						+ localparaMdas);
 		driver.navigate().to("https://www.google.in");
 		driver.manage().window().maximize();
 		// The comment
@@ -32,7 +38,7 @@ public class GoogleTest {
 
 	public void remoteTest() {
 		// URL of the Selenium Grid Hub
-		String hubUrl = "http://192.168.31.138:4444/wd/hub"; 
+		String hubUrl = "http://192.168.31.138:4444/wd/hub";
 
 		// Desired capabilities for the browser (Chrome in this case)
 		ChromeOptions options = new ChromeOptions();
