39850a3d6b341d38be930cfc227aefe3dc673c4a
Mangala DasJenkins <das.prasanna.mangala@gmail.com>
2024-07-17 13:35:32 +0530

    chnages made to run only googletest in testng.xml runner file

--- src/test/java/Amazon/FloraRelease/AppTest.java
+++ src/test/java/Amazon/FloraRelease/AppTest.java
@@ -1,9 +1,14 @@
 package Amazon.FloraRelease;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
@@ -21,11 +26,34 @@ public class AppTest {
 
 	@Test(priority = 0)
 	public void amazonLaunchTest() {
-		driver = new ChromeDriver();
+		boolean remote = false;
+		if (remote)
+			remoteTest();
+
+		else
+			driver = new ChromeDriver();
+
 		driver.navigate().to("https://www.amazon.in");
 		driver.manage().window().maximize();
 		// The comment
 
+	}
+
+	public void remoteTest() {
+		// URL of the Selenium Grid Hub
+		String hubUrl = "http://192.168.31.138:4444/wd/hub";
+
+		// Desired capabilities for the browser (Chrome in this case)
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("browserName", "chrome");
+
+		// Create a RemoteWebDriver instance
+		try {
+			driver = new RemoteWebDriver(new URL(hubUrl), options);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Test(priority = 1)
